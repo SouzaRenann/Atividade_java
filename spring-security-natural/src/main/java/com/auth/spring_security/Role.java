fa709b3fc961d8d7e2ca2da8ba758245c842dbee
Renan Souza <renaninacio506@gmail.com>
2026-05-11 21:32:38 -0300

    Delete spring-security-natural directory

--- spring-security-natural/src/main/java/com/auth/spring_security/Role.java
+++
@@ -1,6 +0,0 @@
-package com.auth.spring_security;
-
-public enum Role {
-    ADMIN,
-    USER
-}
--- spring-security-natural/src/main/java/com/auth/spring_security/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.auth.spring_security.repository;
-
-import com.auth.spring_security.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
